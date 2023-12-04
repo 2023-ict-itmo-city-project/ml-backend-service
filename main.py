017b99bd6e38600d7050e7ab968a8df4508fa5d6
daryabaldina1702 <ddbaldina@mail.ru>
2023-12-04 19:00:59 +0300

    Реализация ML сервиса

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ BASE_URL = "https://https://city24.civiltechgroup.ru/"
 def generate_file_url(issue: IssueDto):
     return f"{BASE_URL}api/v0/issues/{issue.id}/downloadFile"
 
+ 
 
 @app.post("/")
 async def analyze_recommendation(
         issue: IssueDto
 ):
+    token = "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"
     file_url = generate_file_url(issue)
-    ml.some_function(issue, file_url)
-    return ResponseDto(prediction=0.5)
+    #иницилизация модели
+    model = ml.init_model(token, issue.categoryId)
+    #получение распознанных объектов
+    labels = ml.detection(model, file_url)
+    #получение коэффициента доверия
+    coef_of_trust = ml.get_response(token, issue.description, labels, issue.categoryId)
+    return ResponseDto(prediction=coef_of_trust)
